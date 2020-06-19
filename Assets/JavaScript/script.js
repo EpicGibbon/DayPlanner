1aaeef51630ecb16dce64bd72b0a615a89862bf5
Anthony Aguiar <michant1997@gmail.com>
2020-06-19 19:27:04 -0400

    Just need the logic done now

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,35 +1,41 @@
 // 1- Display current time using moment.js
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
-//2 - Click event to save text area content to locaL STORAGE
-$("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
-moment().format("YYYY-MM-DD HH:mm");
+$("#currentDay").text(moment().format('dddd, MMM Do'));
 
-// var dayPlan = description
-// localStorage.setItem("textarea", textarea);
-// console.log(localStorage);
-
-//Saving the inputs into a variable
-// var dayPlan = JSON.parse(localStorage.getItem("textarea"));
-
-// $(".saveBtn").click(function() {
-//     var dayPlan = $(this);  //get the card that is clicked
-//     var storage = description.attr("class");  //get the class name of this card
-//     description.fadeOut();  //hide it
-//     localStorage.setItem("description", storage);  //save the value
-//     console.log(description);
-//   });
-
-
-//When save is clicked, it saves it to local storage
-$(".saveBtn").click(function(event) {
-    event.preventDefault();
-    //Grabbing the previous elements value
-  var prevVal = $(this).prev("textarea").val();
-  console.log(prevVal);
-
-    // storeSave();
+function timeSlot() {
+  const presentTime = moment.hours();
+  $(".row").each(function () {
+    const planRow = parseInt($(this).attr("id"));
+    if (planRow < presentTime) {
+      $(this).addClass("past");
+    }
+    else if (planRow === presentTime) {
+      $(this).addClass("present");
+    }
+    else {
+      $(this).addClass("future");
+    }
   });
+}
+//When save is clicked, it saves it to local storage
+$(".saveBtn").click(function (event) {
+  event.preventDefault();
+  //Grabbing the previous elements value
+  var prevVal = $(this).prev("textarea").val();
+  var time = $(this).attr("data-time");
+  localStorage.setItem(time, prevVal);
+  // storeSave();
+});
 //3 - When the app opens, display content for each time block from local storage
+$("#text-nine").val(localStorage.getItem("9"));
+$("#text-ten").val(localStorage.getItem("10"));
+$("#text-eleven").val(localStorage.getItem("11"));
+$("#text-twelve").val(localStorage.getItem("12"));
+$("#text-one").val(localStorage.getItem("1"));
+$("#text-two").val(localStorage.getItem("2"));
+$("#text-three").val(localStorage.getItem("3"));
+$("#text-four").val(localStorage.getItem("4"));
+$("#text-five").val(localStorage.getItem("5"));
+
     // It can be done with a for loop(more efficient) or by targetting each time block seperartly
 
 //4 - change time block background color depending in the current hour (past, present and future).
