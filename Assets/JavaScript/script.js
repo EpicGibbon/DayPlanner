41b1f7498be9ee61c124b6e1836332ba42a9c46b
Anthony Aguiar <michant1997@gmail.com>
2020-06-18 22:03:26 -0400

    The initial start of this application. The foundation has been set

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,9 @@
+// 1- Display current time using moment.js
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+//2 - Click event to save text area content to locaL STORAGE
+
+//3 - When the app opens, display content for each time block from local storage
+    // It can be done with a for loop(more efficient) or by targetting each time block seperartly
+
+//4 - change time block background color depending in the current hour (past, present and future).
+
