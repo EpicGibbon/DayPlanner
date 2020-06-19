f3e47c16f89e19df96cbdbab145146efd4df1727
Anthony Aguiar <michant1997@gmail.com>
2020-06-19 15:07:42 -0400

    We have the textarea done

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -4,28 +4,30 @@ console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 $("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
 moment().format("YYYY-MM-DD HH:mm");
 
-var dayPlan = description
-localStorage.setItem("textarea", textarea);
-console.log(localStorage);
+// var dayPlan = description
+// localStorage.setItem("textarea", textarea);
+// console.log(localStorage);
 
 //Saving the inputs into a variable
-var dayPlan = JSON.parse(localStorage.getItem("textarea"));
+// var dayPlan = JSON.parse(localStorage.getItem("textarea"));
 
-$("description").click(function() {
-    var dayPlan = $(this);  //get the card that is clicked
-    var storage = description.attr("class");  //get the class name of this card
-    description.fadeOut();  //hide it
-    localStorage.setItem("description", storage);  //save the value
-    console.log(description);
-  });
+// $(".saveBtn").click(function() {
+//     var dayPlan = $(this);  //get the card that is clicked
+//     var storage = description.attr("class");  //get the class name of this card
+//     description.fadeOut();  //hide it
+//     localStorage.setItem("description", storage);  //save the value
+//     console.log(description);
+//   });
+
 
 //When save is clicked, it saves it to local storage
 $(".saveBtn").click(function(event) {
     event.preventDefault();
+    //Grabbing the previous elements value
+  var prevVal = $(this).prev("textarea").val();
+  console.log(prevVal);
 
-
-
-    storeSave();
+    // storeSave();
   });
 //3 - When the app opens, display content for each time block from local storage
     // It can be done with a for loop(more efficient) or by targetting each time block seperartly
