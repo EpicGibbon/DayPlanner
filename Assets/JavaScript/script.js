d95917746b8a4f3adbc81e22d8d5824463c5c555
Anthony Aguiar <michant1997@gmail.com>
2020-06-18 22:57:14 -0400

    Need the javascript functionality

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,7 +1,8 @@
 // 1- Display current time using moment.js
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 //2 - Click event to save text area content to locaL STORAGE
-
+$("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
+moment().format("YYYY-MM-DD HH:mm");
 //3 - When the app opens, display content for each time block from local storage
     // It can be done with a for loop(more efficient) or by targetting each time block seperartly
 
